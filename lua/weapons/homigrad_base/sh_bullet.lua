ea5406a8944bbe65af439cad45d9680a1b488024
uzelezz123 <aleshkakorolkov1@gmail.com>
2026-03-01 16:14:57 +0300

    fix err + ragdoll apply force when shooting

--- lua/weapons/homigrad_base/sh_bullet.lua
+++ lua/weapons/homigrad_base/sh_bullet.lua
@@ -582,6 +582,13 @@ function SWEP:FireBullet()
 		if IsValid(phys) then
 			phys:ApplyForceOffset(-dir * self.Primary.Force * 5, pos)
 		end
+	else
+		local char = hg.GetCurrentCharacter(owner)
+		local phys = char:GetPhysicsObjectNum(0)
+		
+		if IsValid(phys) then
+			phys:ApplyForceOffset(-dir * self.Primary.Force * 40 * (self.NumBullet or 1), pos)
+		end
 	end
 
 	--[[local enta = ents.Create("prop_physics")
