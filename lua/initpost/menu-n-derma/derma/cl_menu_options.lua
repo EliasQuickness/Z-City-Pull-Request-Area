f4e758e41cc5b3cca17c8af3dd4b8f6a9ca314b0
EliasQuickness <131909147+EliasQuickness@users.noreply.github.com>
2026-03-27 11:53:15 -0700

    oops

--- lua/initpost/menu-n-derma/derma/cl_menu_options.lua
+++ lua/initpost/menu-n-derma/derma/cl_menu_options.lua
@@ -66,7 +66,7 @@ if not game.IsDedicated() then
 	hg.settings:AddOpt("Serverside gameplay","hg_healanims", "Heal & food animations")
 	hg.settings:AddOpt("Serverside gameplay","hg_aimtoshoot", "DarkRP-like shoot system (aim to shoot)")
 	hg.settings:AddOpt("Serverside gameplay","hg_slings", "Sling system")
-    hg.settings:AddOpt("Serverside gameplay","homicide_traitoramount", "Homicide: Traitor Amount", nil, nil, "int")
+   	hg.settings:AddOpt("Serverside gameplay","homicide_traitoramount", "Homicide: Traitor Amount", nil, nil, "int")
 end
 --hg_appearance_access_for_all
 --hg_furcity
@@ -90,6 +90,7 @@ hg.settings:AddOpt("Blood","hg_blood_sprites", "Blood Sprites (DISABLED FOR EVER
 hg.settings:AddOpt("Blood","hg_old_blood", "Old blood")
 
 hg.settings:AddOpt("UI","hg_font", "Change Custom Font", false, true)
+hg.settings:AddOpt("UI","hg_nostatic", "Remove static border effects")
 
 hg.settings:AddOpt("Weapons","hg_weaponshotblur_enable", "Shooting Blur")
 hg.settings:AddOpt("Weapons","hg_dynamic_mags", "Dynamic Ammo Inspect")
